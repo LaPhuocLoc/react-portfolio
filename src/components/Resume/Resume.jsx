d7e47e2277bea843c5652386cf9b63614bc69aba
LaPhuocLoc <86074521+LaPhuocLoc@users.noreply.github.com>
2022-05-22 15:12:57 +0900

    Update Resume.jsx

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,28 +4,28 @@ import './resume.scss'
 const edu = [
   {
     time: '2018年7月　卒業',
-    edu: '高等学校',
     place: 'Ly Thuong Kiet High School',
+    edu: '高等学校',
     description: '高校時代に学んでいたプログラミングで課題を解決した際に楽しみを感じたのきっかけで、IT業界に興味を持ちました。プログラミングを学ぶため、日本に来ました。 '
   },
   {
     time: '2019年4月ー2021月3月卒業',
-    edu: '日本語',
     place: '神戸住吉国際日本語学校',
+    edu: '日本語',
     description: '2019年4月から2021年3月まで神戸住吉国際日本語学校で日本語を勉強しました。'
   },
   {
     time: '2021年4月ー2023月3月卒業見込',
-    edu: 'IT・Web学科',
     place: '中央情報専門学校',
+    edu: 'IT・Web学科',
     description: '学校で、Java言語やICT基礎などを学ぶ傍ら、授業以外でも独学でIT技術の学習を続けています。	'
   },
 ]
 const exp = [
   {
     time: '2022年2月26日',
-    exp: 'WEBサイト開発',
     place: 'プレゼンテーショングランプリ大会',
+    exp: 'WEBサイト開発',
     description: '学校のプレゼンテーション大会に参加し、４人のチームでウェブサイトを作り、１年の技術部の2位を受賞することができました。'
   },
 ]
@@ -37,7 +37,7 @@ const Resume = () => {
         学歴 & 経験
       </h3>
       <div className="resume">
-        
+
         <div className="resume__edu" data-aos="fade-right">
           {edu.map((item, i) => (
             <div className="resume__edu__item mb-3" key={i}>
@@ -45,7 +45,7 @@ const Resume = () => {
                 <i className='bx bxs-briefcase' ></i>
               </div>
               <span className="time">{item.time}</span>
-              <h3>{item.edu} <span className="place">{item.place}</span> </h3>
+              <h3>{item.place} <span className="place">{item.edu}</span> </h3>
               <p>{item.description}</p>
             </div>
           ))}
@@ -58,7 +58,7 @@ const Resume = () => {
                 <i className='bx bxs-briefcase' ></i>
               </div>
               <span className="time">{item.time}</span>
-              <h3>{item.exp} <span className="place">{item.place}</span> </h3>
+              <h3>{item.place} <span className="place">{item.exp}</span> </h3>
               <p>{item.description}</p>
             </div>
           ))}
