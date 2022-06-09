cea27fd215d3efd93e418dd3794129cde5f8d344
LA PHUOC LOC <86074521+LaPhuocLoc@users.noreply.github.com>
2022-06-09 16:48:20 +0900

    fix btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './button.scss'
 
 const Button = props => {
+
+  const btnRef = useRef(null)
+
+  const handleHoverIn = () => {
+    btnRef.current.classList.add('is-hover')
+  }
+  const handleHoverOut = () => {
+    btnRef.current.classList.remove('is-hover')
+  }
   return (
-    <button className={`btn ${props.className}`} type={props.type}>
+    <button
+      className={`btn ${props.className}`}
+      type={props.type}
+      onMouseEnter={handleHoverIn}
+      onMouseLeave={handleHoverOut}
+      ref={btnRef}
+    >
       {props.children}
     </button>
   )
