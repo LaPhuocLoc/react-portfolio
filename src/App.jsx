fa482b1c549ba60678eaee30fa161839678f3ed2
LA PHUOC LOC <86074521+LaPhuocLoc@users.noreply.github.com>
2022-06-09 16:12:49 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import About from './pages/About/About';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Contact from './pages/Contact/Contact';
 // import Blog from './pages/Blog/Blog';
-import Cursor from "./components/Cursor/Cursor";
 import ToggleTheme from './components/ToggleTheme/ToggleTheme';
 
 
@@ -37,7 +36,6 @@ function App() {
           {/* <Route path="/blog" element={<Blog />} /> */}
         </Routes>
       </div>
-      {/* <Cursor /> */}
     </BrowserRouter>
   )
 }
